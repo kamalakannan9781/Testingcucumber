fd20494bcdc16f95143927b98208fcec6f5505a1
kamala kannan <kamalakannan9781@gmail.com>
2017-11-21 20:29:33 +0530

    updated reverse string

---
+++ src/programs/ReverseNumber2.java
@@ -0,0 +1,18 @@
+package programs;
+
+import java.util.Scanner;
+
+public class ReverseNumber2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		String value=scanner.next();
+		for(int i=value.length()-1;i>=0;i--)
+		{
+			System.out.println("The reverse of string is :"+value.charAt(i));
+		}
+
+	}
+
+}
