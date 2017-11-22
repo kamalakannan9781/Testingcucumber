4dde942197b85cb73894ce527590486c51e938f4
kamala kannan <kamalakannan9781@gmail.com>
2017-11-22 14:56:47 +0530

    fibonacci changes
    Change-Id: I544761dd3ba7f08c1f19605b732b4cbc4cb832d0
    Signed-off-by: kamala kannan <kamalakannan9781@gmail.com>

---
+++ src/programs/FibonacciSe.java
@@ -0,0 +1,26 @@
+package programs;
+
+import java.util.Scanner;
+
+public class FibonacciSe {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i;
+		Scanner scanner = new Scanner(System.in);
+		int fibocount=scanner.nextInt();
+		int[] fib= new int[fibocount];
+		fib[0]=0;
+		fib[1]=1;
+		for(i=2;i<fibocount;i++)
+		{
+			fib[i]=fib[i-1]+fib[i-2];
+		}
+		for(i=0;i<fibocount;i++)
+		{
+			System.out.println("the fibonacci value is :"+fib[i]);
+		}
+
+	}
+
+}
